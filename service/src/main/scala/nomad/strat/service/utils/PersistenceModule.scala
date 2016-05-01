e645b333c64adc822dff753b340063270374042b
ZoltanVadasz <zvadasz@gmail.com>
2016-05-01 23:46:29 +0200

    Experiment with a generic DAL, not functional yet.

--- service/src/main/scala/nomad/strat/service/utils/PersistenceModule.scala
+++ service/src/main/scala/nomad/strat/service/utils/PersistenceModule.scala
@@ -1,6 +1,9 @@
 package nomad.strat.service.utils
 
-import akka.actor.{ActorPath, ActorSelection, Props, ActorRef}
+import akka.actor.{ActorPath, ActorRef, ActorSelection, Props}
+import nomad.strat.service.persistence.dal.BaseDalImpl
+import nomad.strat.service.persistence.entities.Tables.{Army, ArmyRow, Game, GameRow, PlayerRow, PlayersOfGame, PlayersOfGameRow, StratMap, StratMapRow}
+import nomad.strat.service.rest.Player
 //import persistence.dal._
 import slick.backend.DatabaseConfig
 import slick.driver.{JdbcProfile}
@@ -18,7 +21,13 @@ trait DbModule extends Profile{
 }
 
 trait PersistenceModule {
-	//val suppliersDal: BaseDal[SuppliersTable,Supplier]
+  /*
+	val playerDal: BaseDalImpl[Player,PlayerRow]
+	val gameDal: BaseDalImpl[Game,GameRow]
+	val playersOfGameDal: BaseDalImpl[PlayersOfGame,PlayersOfGameRow]
+	val armyDal: BaseDalImpl[Army,ArmyRow]
+	val mapDal: BaseDalImpl[StratMap,StratMapRow]
+	*/
 }
 
 
@@ -27,13 +36,19 @@ trait PersistenceModuleImpl extends PersistenceModule with DbModule{
 
 	// use an alternative database configuration ex:
 	// private val dbConfig : DatabaseConfig[JdbcProfile]  = DatabaseConfig.forConfig("pgdb")
-	private val dbConfig : DatabaseConfig[JdbcProfile]  = DatabaseConfig.forConfig("h2db")
+	// private val dbConfig : DatabaseConfig[JdbcProfile]  = DatabaseConfig.forConfig("h2db")
+	private val dbConfig : DatabaseConfig[JdbcProfile]  = DatabaseConfig.forConfig("pgsql")
 
 	override implicit val profile: JdbcProfile = dbConfig.driver
 	override implicit val db: JdbcProfile#Backend#Database = dbConfig.db
 
-	//override val suppliersDal = new BaseDalImpl[SuppliersTable,Supplier](TableQuery[SuppliersTable]) {}
-
+  /*
+	override val playerDal = new BaseDalImpl[Player,PlayerRow](TableQuery[Player]) {}
+	override val gameDal = new BaseDalImpl[Game,GameRow](TableQuery[Game]) {}
+	override val playersOfGameDal = new BaseDalImpl[PlayersOfGame,PlayersOfGameRow](TableQuery[PlayersOfGame]) {}
+	override val armyDal = new BaseDalImpl[Army,ArmyRow](TableQuery[Army]) {}
+	override val mapDal = new BaseDalImpl[StratMap,StratMapRow](TableQuery[StratMap]) {}
+*/
 	val self = this
 
 }
